46916315e431146465138868a56a0dfa60a9e0d7
Guilherme Martins <academicgmag@gmail.com>
2023-06-19 00:00:03 -0300

    add pwa configs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,10 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>boilerplate</title>
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
           content="A simple project starter to work with Typescript, React, NextJS and Styled Components"
